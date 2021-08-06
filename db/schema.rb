883f39b5158409377026f3cf4453dd64a3ce1833
Amanda McGuire <aruwart001@gmail.com>
2021-08-06 15:11:44 -0600

    Add discount tables to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,34 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_28_170702) do
+ActiveRecord::Schema.define(version: 2021_08_06_200126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bulk_discounts", force: :cascade do |t|
+    t.integer "percentage"
+    t.integer "quantity"
+    t.bigint "merchant_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["merchant_id"], name: "index_bulk_discounts_on_merchant_id"
+  end
 
   create_table "customers", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "discounted_items", force: :cascade do |t|
+    t.bigint "item_id"
+    t.bigint "bulk_discount_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["bulk_discount_id"], name: "index_discounted_items_on_bulk_discount_id"
+    t.index ["item_id"], name: "index_discounted_items_on_item_id"
   end
 
   create_table "invoice_items", force: :cascade do |t|
@@ -70,6 +88,9 @@ ActiveRecord::Schema.define(version: 2021_07_28_170702) do
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
   end
 
+  add_foreign_key "bulk_discounts", "merchants"
+  add_foreign_key "discounted_items", "bulk_discounts"
+  add_foreign_key "discounted_items", "items"
   add_foreign_key "invoice_items", "invoices"
   add_foreign_key "invoice_items", "items"
   add_foreign_key "invoices", "customers"
