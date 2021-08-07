0ce8805e7aef93c310e993fb3f4b138c25f85e75
Amanda McGuire <aruwart001@gmail.com>
2021-08-06 18:19:58 -0600

    Remove joins table

--- db/migrate/20210806200126_create_discounted_items.rb
+++
@@ -1,9 +0,0 @@
-class CreateDiscountedItems < ActiveRecord::Migration[5.2]
-  def change
-    create_table :discounted_items do |t|
-      t.references :item, foreign_key: true
-      t.references :bulk_discount, foreign_key: true
-      t.timestamps
-    end
-  end
-end
