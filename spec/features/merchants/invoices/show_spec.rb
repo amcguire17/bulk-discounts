c949150db6798f0b0b295bcf5daf2537b2245d3b
Amanda McGuire <aruwart001@gmail.com>
2021-08-08 17:46:25 -0600

    Add discounted revenue total to merchant invoice show page

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -1,27 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Merchant Invoices show page' do
-#   Merchant Invoice Show Page
-#
-# As a merchant
-# When I visit my merchant's invoice show page(/merchants/merchant_id/invoices/invoice_id)
-# Then I see information related to that invoice including:
-# - Invoice id
-# - Invoice status
-# - Invoice created_at date in the format "Monday, July 18, 2019"
-# - Customer first and last name
   before :each do
     @merchant = create(:merchant)
 
     @customer = create(:customer)
 
-    @invoice = create(:invoice, customer_id: @customer.id)
+    @invoice = create(:invoice, customer: @customer)
 
-    @item_1 = create(:item, merchant_id: @merchant.id)
-    @item_2 = create(:item, merchant_id: @merchant.id)
+    @item_1 = create(:item, merchant: @merchant)
+    @item_2 = create(:item, merchant: @merchant)
 
-    @invoice_item_1 = create(:invoice_item, item_id: @item_1.id, invoice_id: @invoice.id, status: 1)
-    @invoice_item_2 = create(:invoice_item, item_id: @item_2.id, invoice_id: @invoice.id, status: 1)
+    @invoice_item_1 = create(:invoice_item, item: @item_1, invoice: @invoice, status: 1)
+    @invoice_item_2 = create(:invoice_item, item: @item_2, invoice: @invoice, status: 1)
+
+    @bulk_discounts = @merchant.bulk_discounts.create!(quantity: 5, percentage: 15)
 
     visit "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
   end
@@ -33,17 +26,6 @@ RSpec.describe 'Merchant Invoices show page' do
     expect(page).to have_content(@customer.first_name)
     expect(page).to have_content(@customer.last_name)
   end
-
-  # Merchant Invoice Show Page: Invoice Item Information
-  #
-  # As a merchant
-  # When I visit my merchant invoice show page
-  # Then I see all of my items on the invoice including:
-  # - Item name
-  # - The quantity of the item ordered
-  # - The price the Item sold for
-  # - The Invoice Item status
-  # And I do not see any information related to Items for other merchants
 
   it "displays the invoice item information such as the item name, quantity ordered, price of item, invoice item status" do
     within "#invoice_item-info-#{@invoice_item_1.id}" do
@@ -61,28 +43,14 @@ RSpec.describe 'Merchant Invoices show page' do
     end
   end
 
-  # Merchant Invoice Show Page: Total Revenue
-  #
-  # As a merchant
-  # When I visit my merchant invoice show page
-  # Then I see the total revenue that will be generated from all of my items on the invoice
-
   it "displays the total revenue from all items on the invoice" do
     expect(page).to have_content("Total revenue from invoice: #{@invoice.total_revenue}")
   end
 
-#   Merchant Invoice Show Page: Update Item Status
-#
-# As a merchant
-# When I visit my merchant invoice show page
-# I see that each invoice item status is a select field
-# And I see that the invoice item's current status is selected
-# When I click this select field,
-# Then I can select a new status for the Item,
-# And next to the select field I see a button to "Update Item Status"
-# When I click this button
-# I am taken back to the merchant invoice show page
-# And I see that my Item's status has now been updated
+  it "displays the total discounted revenue from all items on the invoice" do
+    expect(page).to have_content("Total discounted revenue from invoice: #{@invoice.total_discounted_revenue}")
+  end
+
   it 'can click on the select status for an item and update it to a new status' do
     within "#invoice_item-info-#{@invoice_item_1.id}" do
 
