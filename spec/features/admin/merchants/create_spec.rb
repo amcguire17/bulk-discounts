d5eed01b16d53c16d2b5a63887e8bb460b872502
Amanda McGuire <aruwart001@gmail.com>
2021-08-09 14:47:27 -0600

    Update admin merchants specs

--- spec/features/admin/merchants/create_spec.rb
+++ spec/features/admin/merchants/create_spec.rb
@@ -1,17 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe 'admin merchant create page' do
-  it 'admin merchant index page has link to create new merchant' do
+  before :each do
     visit admin_merchants_path
-
+  end
+  it 'admin merchant index page has link to create new merchant' do
     click_link 'New Merchant'
-
     expect(current_path).to eq(new_admin_merchant_path)
   end
 
   it 'can create a new merchant and defaults to disabled' do
-    visit new_admin_merchant_path
-
     fill_in 'Name', with: 'Chuck Norris'
     click_button('Create Merchant')
 
--- spec/features/admin/merchants/index_spec.rb
+++ spec/features/admin/merchants/index_spec.rb
@@ -2,163 +2,106 @@ require 'rails_helper'
 
 RSpec.describe 'admin merchants index page' do
   before :each do
-    @merchant_1 = create(:merchant, status: 'enabled')
-    @merchant_2 = create(:merchant)
-    @merchant_3 = create(:merchant, status: 'enabled')
-    @merchant_4 = create(:merchant)
-  end
-
-  it 'displays all of the merchants' do
+    @merchant_1, @merchant_3 = create_list(:merchant, 2, status: 'enabled')
+    @merchant_2, @merchant_4 = create_list(:merchant, 2)
     visit admin_merchants_path
-
-    expect(page).to have_content(@merchant_1.name)
-    expect(page).to have_content(@merchant_2.name)
-    expect(page).to have_content(@merchant_3.name)
-    expect(page).to have_content(@merchant_4.name)
   end
-
-  it 'displays a button to endable merchant with disabled status and update status' do
-    visit admin_merchants_path
-
-    within ("#merchant-#{@merchant_2.id}") do
-      click_button('Enable')
-    end
-
-    expect(current_path).to eq(admin_merchants_path)
-
-    within ("#merchant-#{@merchant_2.id}") do
-      expect(page).to_not have_button('Enable')
-      expect(page).to have_button('Disable')
-    end
-  end
-
-  it 'displays a button to disable merchant with enabled status and update status' do
-    visit admin_merchants_path
-
-    within ("#merchant-#{@merchant_1.id}") do
-      click_button('Disable')
-    end
-
-    expect(current_path).to eq(admin_merchants_path)
-
-    within ("#merchant-#{@merchant_1.id}") do
-      expect(page).to_not have_button('Disable')
-      expect(page).to have_button('Enable')
-    end
-  end
-
-  it 'groups merchants by status' do
-    visit admin_merchants_path
-
-    within ("#enabled") do
+  describe 'merchant display' do
+    it 'shows all of the merchants' do
       expect(page).to have_content(@merchant_1.name)
-      expect(page).to have_content(@merchant_3.name)
-    end
-
-    within ("#disabled") do
       expect(page).to have_content(@merchant_2.name)
+      expect(page).to have_content(@merchant_3.name)
       expect(page).to have_content(@merchant_4.name)
     end
-  end
+    it 'shows a button to endable merchant with disabled status and update status' do
+      within ("#merchant-#{@merchant_2.id}") do
+        click_button('Enable')
+      end
 
-  it 'displays top five merchants by revenue' do
-    merchant_5 = create(:merchant, status: 'enabled')
-    merchant_6 = create(:merchant)
-    merchant_7 = create(:merchant)
+      expect(current_path).to eq(admin_merchants_path)
 
-    item_1 = create(:item, merchant_id: @merchant_1.id)
-    item_2 = create(:item, merchant_id: @merchant_1.id)
-    item_3 = create(:item, merchant_id: @merchant_3.id)
-    item_4 = create(:item, merchant_id: @merchant_3.id)
-    item_5 = create(:item, merchant_id: @merchant_4.id)
-    item_6 = create(:item, merchant_id: @merchant_4.id)
-    item_7 = create(:item, merchant_id: merchant_5.id)
-    item_8 = create(:item, merchant_id: merchant_5.id)
-    item_9 = create(:item, merchant_id: merchant_6.id)
-    item_10 = create(:item, merchant_id: merchant_7.id)
-
-    invoice_1 = create(:invoice)
-    invoice_2 = create(:invoice)
-    invoice_3 = create(:invoice)
-    invoice_4 = create(:invoice)
-    invoice_5 = create(:invoice)
-    invoice_6 = create(:invoice)
-
-    transactions_1 = create(:transaction, invoice_id: invoice_1.id)
-    transactions_2 = create(:transaction, invoice_id: invoice_2.id)
-    transactions_3 = create(:transaction, invoice_id: invoice_3.id)
-    transactions_4 = create(:transaction, invoice_id: invoice_4.id)
-    transactions_5 = create(:transaction, invoice_id: invoice_5.id)
-    transactions_6 = create(:transaction, invoice_id: invoice_6.id)
-
-    invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 12000, item_id: item_1.id, invoice_id: invoice_1.id)
-    invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 10100, item_id: item_2.id, invoice_id: invoice_1.id)
-    invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 20000, item_id: item_10.id, invoice_id: invoice_2.id)
-    invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 15000, item_id: item_4.id, invoice_id: invoice_3.id)
-    invoice_item_1 = create(:invoice_item, quantity: 3, unit_price: 15000, item_id: item_3.id, invoice_id: invoice_4.id)
-    invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 9000, item_id: item_5.id, invoice_id: invoice_5.id)
-    invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 23000, item_id: item_9.id, invoice_id: invoice_6.id)
-
-    visit admin_merchants_path
-
-    within ("#top_revenue") do
-      expect(@merchant_3.name).to appear_before(@merchant_1.name)
-      expect(merchant_7.name).to_not appear_before(merchant_6.name)
-      expect(@merchant_1.name).to appear_before(@merchant_4.name)
-      expect(page).to_not have_content(@merchant_2.name)
+      within ("#merchant-#{@merchant_2.id}") do
+        expect(page).to_not have_button('Enable')
+        expect(page).to have_button('Disable')
+      end
     end
+    it 'displays a button to disable merchant with enabled status and update status' do
+      within ("#merchant-#{@merchant_1.id}") do
+        click_button('Disable')
+      end
 
-    within ("#top_revenue") do
-      expect(page).to have_content("#{@merchant_1.name} - $221.0 in sales")
-      expect(page).to have_content("#{@merchant_3.name} - $750.0 in sales")
-      expect(page).to have_content("#{@merchant_4.name} - $90.0 in sales")
+      expect(current_path).to eq(admin_merchants_path)
+
+      within ("#merchant-#{@merchant_1.id}") do
+        expect(page).to_not have_button('Disable')
+        expect(page).to have_button('Enable')
+      end
+    end
+    it 'groups merchants by status' do
+      within ("#enabled") do
+        expect(page).to have_content(@merchant_1.name)
+        expect(page).to have_content(@merchant_3.name)
+      end
+
+      within ("#disabled") do
+        expect(page).to have_content(@merchant_2.name)
+        expect(page).to have_content(@merchant_4.name)
+      end
     end
   end
+  describe 'top revenue earners' do
+    before :each do
+      @merchant_5 = create(:merchant, status: 'enabled')
+      @merchant_6, @merchant_7 = create_list(:merchant, 2)
 
-  it 'dispalys the top merchants best revenue day' do
-    merchant_5 = create(:merchant, status: 'enabled')
-    merchant_6 = create(:merchant)
-    merchant_7 = create(:merchant)
+      @item_1, @item_2 = create_list(:item, 2, merchant: @merchant_1)
+      @item_3, @item_4 = create_list(:item, 2, merchant: @merchant_3)
+      @item_5, @item_6 = create_list(:item, 2, merchant: @merchant_4)
+      @item_7, @item_8 = create_list(:item, 2, merchant: @merchant_5)
+      @item_9 = create(:item, merchant: @merchant_6)
+      @item_10 = create(:item, merchant: @merchant_7)
 
-    item_1 = create(:item, merchant_id: @merchant_1.id)
-    item_2 = create(:item, merchant_id: @merchant_1.id)
-    item_3 = create(:item, merchant_id: @merchant_3.id)
-    item_4 = create(:item, merchant_id: @merchant_3.id)
-    item_5 = create(:item, merchant_id: @merchant_4.id)
-    item_6 = create(:item, merchant_id: @merchant_4.id)
-    item_7 = create(:item, merchant_id: merchant_5.id)
-    item_8 = create(:item, merchant_id: merchant_5.id)
-    item_9 = create(:item, merchant_id: merchant_6.id)
-    item_10 = create(:item, merchant_id: merchant_7.id)
+      @invoice_1 = create(:invoice, status: 'completed', created_at: '2019-06-24 21:54:10 UTC')
+      @invoice_2 = create(:invoice, status: 'completed', created_at: '2019-07-24 21:54:10 UTC')
+      @invoice_3 = create(:invoice, status: 'completed', created_at: '2019-08-24 21:54:10 UTC')
+      @invoice_4 = create(:invoice, status: 'completed', created_at: '2019-11-24 21:54:10 UTC')
+      @invoice_5 = create(:invoice, status: 'completed', created_at: '2019-01-24 21:54:10 UTC')
+      @invoice_6 = create(:invoice, status: 'completed', created_at: '2019-07-24 21:54:10 UTC')
 
-    invoice_1 = create(:invoice, status: 'completed', created_at: '2019-06-24 21:54:10 UTC')
-    invoice_2 = create(:invoice, status: 'completed', created_at: '2019-07-24 21:54:10 UTC')
-    invoice_3 = create(:invoice, status: 'completed', created_at: '2019-08-24 21:54:10 UTC')
-    invoice_4 = create(:invoice, status: 'completed', created_at: '2019-11-24 21:54:10 UTC')
-    invoice_5 = create(:invoice, status: 'completed', created_at: '2019-01-24 21:54:10 UTC')
-    invoice_6 = create(:invoice, status: 'completed', created_at: '2019-07-24 21:54:10 UTC')
+      Invoice.all.each do |invoice|
+        create(:transaction, invoice: invoice)
+      end
 
-    transactions_1 = create(:transaction, invoice_id: invoice_1.id)
-    transactions_2 = create(:transaction, invoice_id: invoice_2.id)
-    transactions_3 = create(:transaction, invoice_id: invoice_3.id)
-    transactions_4 = create(:transaction, invoice_id: invoice_4.id)
-    transactions_5 = create(:transaction, invoice_id: invoice_5.id)
-    transactions_6 = create(:transaction, invoice_id: invoice_6.id)
+      @invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 12000, item: @item_1, invoice: @invoice_1)
+      @invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 10100, item: @item_2, invoice: @invoice_1)
+      @invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 20000, item: @item_10, invoice: @invoice_2)
+      @invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 15000, item: @item_4, invoice: @invoice_3)
+      @invoice_item_1 = create(:invoice_item, quantity: 3, unit_price: 15000, item: @item_3, invoice: @invoice_4)
+      @invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 9000, item: @item_5, invoice: @invoice_5)
+      @invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 23000, item: @item_9, invoice: @invoice_6)
+      visit admin_merchants_path
+    end
+    it 'displays top five merchants by revenue' do
+      within ("#top_revenue") do
+        expect(@merchant_3.name).to appear_before(@merchant_1.name)
+        expect(@merchant_7.name).to_not appear_before(@merchant_6.name)
+        expect(@merchant_1.name).to appear_before(@merchant_4.name)
+        expect(page).to_not have_content(@merchant_2.name)
+      end
 
-    invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 12000, item_id: item_1.id, invoice_id: invoice_1.id)
-    invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 10100, item_id: item_2.id, invoice_id: invoice_1.id)
-    invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 20000, item_id: item_10.id, invoice_id: invoice_2.id)
-    invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 15000, item_id: item_4.id, invoice_id: invoice_3.id)
-    invoice_item_1 = create(:invoice_item, quantity: 3, unit_price: 15000, item_id: item_3.id, invoice_id: invoice_4.id)
-    invoice_item_1 = create(:invoice_item, quantity: 1, unit_price: 9000, item_id: item_5.id, invoice_id: invoice_5.id)
-    invoice_item_1 = create(:invoice_item, quantity: 2, unit_price: 23000, item_id: item_9.id, invoice_id: invoice_6.id)
-
-    visit admin_merchants_path
-    within ("#top_revenue") do
-      expect(page).to have_content("Top day for #{@merchant_1.name} was 06/24/19")
-      expect(page).to have_content("Top day for #{@merchant_3.name} was 11/24/19")
-      expect(page).to have_content("Top day for #{@merchant_4.name} was 01/24/19")
-      expect(page).to have_content("Top day for #{merchant_6.name} was 07/24/19")
+      within ("#top_revenue") do
+        expect(page).to have_content("#{@merchant_1.name} - $221.0 in sales")
+        expect(page).to have_content("#{@merchant_3.name} - $750.0 in sales")
+        expect(page).to have_content("#{@merchant_4.name} - $90.0 in sales")
+      end
+    end
+    it 'dispalys the top merchants best revenue day' do
+      within ("#top_revenue") do
+        expect(page).to have_content("Top day for #{@merchant_1.name} was 06/24/19")
+        expect(page).to have_content("Top day for #{@merchant_3.name} was 11/24/19")
+        expect(page).to have_content("Top day for #{@merchant_4.name} was 01/24/19")
+        expect(page).to have_content("Top day for #{@merchant_6.name} was 07/24/19")
+      end
     end
   end
 end
