d6077f08b932fb6ca1bfb3abed915950207bbc35
Amanda McGuire <aruwart001@gmail.com>
2021-08-10 13:57:27 -0600

    Add find holiday discount method

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -3,4 +3,8 @@ class BulkDiscount < ApplicationRecord
   validates :percentage, numericality: { less_than: 100 }
   validates :quantity, numericality: { only_integer: true }
   belongs_to :merchant
+
+  def self.find_holiday_discount(holiday)
+    find_by('name ILIKE ?', "%#{holiday}%")
+  end
 end
--- spec/models/bulk_discount_spec.rb
+++ spec/models/bulk_discount_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe BulkDiscount do
     it { should validate_numericality_of(:percentage).is_less_than(100) }
     it { should validate_numericality_of(:quantity) }
   end
+  describe 'class methods' do
+    describe '.find_holiday_discount' do
+      it 'returns holiday discount' do
+        merchant = create(:merchant)
+        bulk_discount = merchant.bulk_discounts.create!(name: 'Labour Day Discount', quantity: 5, percentage: 25)
+        holiday = 'Labour Day'
+        expect(BulkDiscount.find_holiday_discount(holiday)).to eq(bulk_discount)
+      end
+    end
+  end
 end
